3db97cda49f55010da0b1c3889923f77652aa87d
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-07-14 12:17:14 +0900

    Test the assembly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::arch::asm;
+
 fn main() {
-    println!("Hello, world!");
+    let mut result: u64;
+    let a: u64 = 5;
+    let b: u64 = 3;
+
+    unsafe {
+        asm!(
+            "add {0}, {1}, {2}",
+            out(reg) result,
+            in(reg) a,
+            in(reg) b
+        );
+    }
+
+    println!("Result of {} + {} is: {}", a, b, result);
 }
