6beca101886b622d5d26ad5fee20e4dcb1ad418d
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-07-19 01:10:22 +0900

    Implement memory copy

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,10 @@
-pub type Uint8T = u8;
-pub type Uint32T = u32;
-pub type SizeT = Uint32T;
+type Paddr = u32;
+type Vaddr = u32;
 
 extern "C" {
-    pub static mut __bss: Uint8T;
-    pub static mut __bss_end: Uint8T;
-    pub static mut __stack_top: Uint8T;
+    pub static mut __bss: u8;
+    pub static mut __bss_end: u8;
+    pub static mut __stack_top: u8;
 }
 
 #[repr(C)]
